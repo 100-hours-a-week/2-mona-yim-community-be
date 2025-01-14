8804e3ae52dfb2f77aa964502b4f2890c2f2d872
younw00 <younwoo@umich.edu>
2025-01-14 19:21:20 +0900

    fix: s3 delete image

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -59,27 +59,6 @@ const uploadFileToS3 = async (file) => {
     return imageName;
 };
 
-// S3 삭제 함수
-export async function deleteImage(imageName) {
-    console.log('들어옴');
-    if (imageName === null) return;
-    // S3에서 삭제할 Key 설정
-    const key = `uploads/${imageName}`;
-    console.log(key);
-    const params = {
-        Bucket: process.env.AWS_S3_BUCKET_NAME,
-        Key: key,
-    };
-
-    try {
-        const command = new DeleteObjectCommand(params);
-        await s3.send(command);
-        console.log('S3 파일 삭제 성공:', key);
-    } catch (error) {
-        console.error('S3 파일 삭제 실패:', error);
-    }
-}
-
 const router = express.Router();
 
 // S3 업로드 미들웨어 통합
@@ -132,5 +111,26 @@ router.get('/posts/:id/comments', getComments);
 router.post('/posts/:id/comments', uploadComment);
 router.patch('/posts/:id/comments/:commentId', editComment);
 router.delete('/posts/:id/comments/:commentId', removeComment);
+
+// S3 삭제 함수
+export async function deleteImage(imageName) {
+    console.log('들어옴');
+    if (imageName === null) return;
+    // S3에서 삭제할 Key 설정
+    const key = `uploads/${imageName}`;
+    console.log(key);
+    const params = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: key,
+    };
+
+    try {
+        const command = new DeleteObjectCommand(params);
+        await s3.send(command);
+        console.log('S3 파일 삭제 성공:', key);
+    } catch (error) {
+        console.error('S3 파일 삭제 실패:', error);
+    }
+}
 
 export default router;

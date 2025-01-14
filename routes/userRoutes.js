7b1bdeb3ee57e14c08cc8ae503f97b3ba10e59c9
younw00 <younwoo@umich.edu>
2025-01-14 14:57:29 +0900

    fix: s3 image upload test

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,53 +2,34 @@
 import express from 'express';
 import multer from 'multer';
 import multerS3 from 'multer-s3';
-
-import {
-    selfInfo,
-    userInfo,
-    loginUser,
-    signinUser,
-    usernameCheck,
-    emailCheck,
-    editProfile,
-    editPassword,
-    removeUser,
-    logoutUser,
-} from '../controllers/userController.js';
-
+import AWS from 'aws-sdk';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
+// AWS S3 설정
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION,
 });
 
+// Multer 설정
 const upload = multer({
     storage: multerS3({
         s3,
         bucket: process.env.AWS_S3_BUCKET_NAME,
         acl: 'public-read',
         metadata: (req, file, cb) => {
+            cb(null, { fieldName: file.fieldname });
+        },
+        key: (req, file, cb) => {
             const uniqueSuffix =
                 Date.now() + '-' + Math.round(Math.random() * 1e9);
-            cb(null, uniqueSuffix + '-' + file.originalname);
+            cb(null, `uploads/${uniqueSuffix}-${file.originalname}`);
         },
     }),
 });
-
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, 'images/');
-//     },
-//     filename: (req, file, cb) => {
-//         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-//         cb(null, uniqueSuffix + '-' + file.originalname);
-//     },
-// });
-
-// const upload = multer({ storage });
 
 const router = express.Router();
 
